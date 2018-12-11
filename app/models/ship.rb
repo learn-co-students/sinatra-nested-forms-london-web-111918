20612204a026c5f9820d53ae5011fdd4104efdee
Lingy94 <j.lingyx@gmail.com>
2018-12-11 15:24:33 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize(ship_detail)
+      @name = ship_detail[:name]
+      @type = ship_detail[:type]
+      @booty = ship_detail[:booty]
+      @@all << self
+    end
+
+      def self.all
+        @@all
+      end
+
+      def self.clear
+        @@all.clear
+      end
 end
